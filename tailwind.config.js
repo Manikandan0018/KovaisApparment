21c82daf0cffa699acee10232c5a516148edd05e
manikandan <manikandan110305@gmail.com>
2025-06-27 20:46:09 +0530

    tailwind some change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default  {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-    theme: {
+   safelist: [
+    "block", "hidden", "flex", "lg:flex", "lg:hidden", "lg:items-center", "lg:w-auto",
+    "flex-col", "flex-row", "space-y-2", "lg:space-x-6", "lg:space-y-0"
+  ],  
+  theme: {
     extend: {
      fontFamily: {
   playfair: ['"Playfair Display"', 'serif'],
